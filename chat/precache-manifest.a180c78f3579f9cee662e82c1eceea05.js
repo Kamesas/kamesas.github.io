bbec5188eb7800e7cb20846c39568ddd7ed08a2a
Alexandr Semak <semakaleksandr2014@gmail.com>
2019-05-06 19:05:13 +0300

    Add files via upload

---
+++ chat/precache-manifest.a180c78f3579f9cee662e82c1eceea05.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = [
+  {
+    "revision": "4c2c81e4fcd6351c0d26",
+    "url": "/static/js/5.f7e42117.chunk.js"
+  },
+  {
+    "revision": "8e1616436ca3eb6e332b",
+    "url": "/static/css/main.e16bbd3a.chunk.css"
+  },
+  {
+    "revision": "d14044af52a9fc06aa40",
+    "url": "/static/js/runtime~main.ad56a97d.js"
+  },
+  {
+    "revision": "9508b3e097f5d4db1edd",
+    "url": "/static/js/2.97807068.chunk.js"
+  },
+  {
+    "revision": "a607b2ec5daa1abf9ba5cfc3f1b5de4c",
+    "url": "/static/media/sprites_v1.a607b2ec.png"
+  },
+  {
+    "revision": "34dbe044dc6011070af9",
+    "url": "/static/js/3.babb3b27.chunk.js"
+  },
+  {
+    "revision": "713869fbaf4b51ca62d0",
+    "url": "/static/js/8.e4c467ef.chunk.js"
+  },
+  {
+    "revision": "80f47ef8878f9971dc73",
+    "url": "/static/js/4.ea5d3030.chunk.js"
+  },
+  {
+    "revision": "725a7c0ac733789c469e",
+    "url": "/static/js/7.13a1d20b.chunk.js"
+  },
+  {
+    "revision": "8e1616436ca3eb6e332b",
+    "url": "/static/js/main.6ebb471b.chunk.js"
+  },
+  {
+    "revision": "abb16785d3535ef64105",
+    "url": "/static/js/6.2994eec0.chunk.js"
+  },
+  {
+    "revision": "713869fbaf4b51ca62d0",
+    "url": "/static/css/8.24c9bb40.chunk.css"
+  },
+  {
+    "revision": "725a7c0ac733789c469e",
+    "url": "/static/css/7.60f082ae.chunk.css"
+  },
+  {
+    "revision": "abb16785d3535ef64105",
+    "url": "/static/css/6.7017ec2c.chunk.css"
+  },
+  {
+    "revision": "4c2c81e4fcd6351c0d26",
+    "url": "/static/css/5.a881c59a.chunk.css"
+  },
+  {
+    "revision": "80f47ef8878f9971dc73",
+    "url": "/static/css/4.a3cc4ff6.chunk.css"
+  },
+  {
+    "revision": "34dbe044dc6011070af9",
+    "url": "/static/css/3.30619749.chunk.css"
+  },
+  {
+    "revision": "fe3a8ae987a9c511367b116f3c71d4aa",
+    "url": "/index.html"
+  }
+];
