84532658b88533b9c6e9313dab526045f2f7b52f
Kamesas <semakaleksandr2014@gmail.com>
2022-02-05 05:55:49 +0200

    add Pomodora app init

---
+++ React/Pomodoro/src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <img src={logo} alt="logo" />
+      Pomodoro Timer
+    </div>
+  );
+}
+
+export default App;
